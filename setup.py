d73b4158fdec2d15759b1d6f01269d01a7c10c6f
Deathdater <aditya.juet@gmail.com>
2019-07-30 22:46:17 +0530

    Added path functionality to take inputs from user. Printing Stockwise info Searching based on the name and printing the stock data. Removing Extra Commented code.
    Dummy Buy Sell logic implemented
    More cleaner presentation.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='StockValidator',
+    version='v0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Deathdater',
+    author_email='',
+    description='This validates a stock '
+)
